535be25b606b254356231f2f010a43b8963a860f
ehu29 <ehu29@knox.nsw.edu.au>
2026-08-29 16:30:02 +0000

    add match auton skeletons

---
+++ Override-Code/include/match_autons.hpp
@@ -0,0 +1,4 @@
+void Q1_L_T2P();
+void Q1_R_T4P();
+void Q2_L_T4P();
+void Q2_R_T2P();
---
+++ Override-Code/src/match_autons.cpp
@@ -0,0 +1,21 @@
+#include "robot.hpp"
+
+void Q1_L_T2P()
+{
+
+}
+
+void Q1_R_T4P()
+{
+
+}
+
+void Q2_L_T4P()
+{
+
+}
+
+void Q3_R_T2P()
+{
+    
+}
