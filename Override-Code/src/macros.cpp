b24037e7d4afb6797df0ed9cf7c42dc6a7012a68
Lucas <lzhang29@knox.nsw.edu.au>
2026-09-12 22:38:28 +1000

    add move up to intake pos macro

--- Override-Code/src/macros.cpp
+++ Override-Code/src/macros.cpp
@@ -13,7 +13,8 @@ void intake_position_macro()
 {
     release_grip();
 
+    position_control::move_relative_degrees_blocking(position_control::MotorId::Lift, 500, 600, 600);
     position_control::move_absolute_degrees_blocking(position_control::MotorId::Arm, -610, 600, 800);
     position_control::move_absolute_blocking(position_control::MotorId::Lift, 0, 12000, 1500);
-    position_control::move_absolute_degrees_blocking(position_control::MotorId::Arm, -20, 600, 800);
+    position_control::move_absolute_degrees_blocking(position_control::MotorId::Arm, 0, 600, 800);
 }
